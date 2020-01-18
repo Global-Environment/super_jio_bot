e6b2e04799217708bfb0248a6c457945982da6bd
Khairul <e0309879@u.nus.edu>
2020-01-18 15:53:21 +0800

    Initialised bot

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Telegraf = require('telegraf')
+require('dotenv').config()
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply('Welcome'))
@@ -6,3 +7,5 @@ bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 bot.launch()
+
+console.log('Telegram bot is active')
