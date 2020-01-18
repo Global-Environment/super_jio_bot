6ce56509dc7a1c9614eee04b117731986f5e1879
Khairul <e0309879@u.nus.edu>
2020-01-18 16:31:56 +0800

    Add menu items

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const Telegraf = require('telegraf')
+const TelegrafInlineMenu = require('telegraf-inline-menu')
 require('dotenv').config()
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome'))
+
+bot.start((ctx) => ctx.reply('Welcome to Super Jio!'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+
+const mainMenu = new TelegrafInlineMenu(ctx => `Hi ${ctx.from.first_name}!`)
+
+mainMenu.setCommand('start')
+
+mainMenu.simpleButton('Start a Jio!', 'a', {
+    doFunc: ctx => ctx.reply('Working On it....')
+})
+
+mainMenu.simpleButton('Add new Restaurant', 'b',{
+    doFunc: ctx => ctx.reply('Working On it....')
+})
+
+mainMenu.simpleButton('Statistics', 'c', {
+    doFunc: ctx => ctx.reply('Working on it....')
+})
+
 bot.launch()
+
+bot.use(mainMenu.init())
 
 console.log('Telegram bot is active')
